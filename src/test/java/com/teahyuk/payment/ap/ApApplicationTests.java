9f01d267d649d00c57809471ab3aed034fe4d585
teahyuk <jtaeh57@gmail.com>
2020-06-28 18:04:11 +0900

    ApApplicationTest RandomPort 설정 추가
    - Context load 잘 되는지 확인하는 테스트에서 port binding 충돌 피하기위해 추가

--- src/test/java/com/teahyuk/payment/ap/ApApplicationTests.java
+++ src/test/java/com/teahyuk/payment/ap/ApApplicationTests.java
@@ -3,7 +3,7 @@ package com.teahyuk.payment.ap;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApApplicationTests {
 
 	@Test
