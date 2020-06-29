cf35af33e71c534a8862b69908b1a55d0f66b860
정태혁 <jtaeh57@gmail.com>
2020-06-29 13:47:29 +0900

    CardInfo 에 encrypted 관련 메소드 추가

--- src/main/java/com/teahyuk/payment/ap/domain/CardInfo.java
+++ src/main/java/com/teahyuk/payment/ap/domain/CardInfo.java
@@ -1,5 +1,7 @@
 package com.teahyuk.payment.ap.domain;
 
+import com.teahyuk.payment.ap.util.AES256Crypto;
+import com.teahyuk.payment.ap.util.CryptoException;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -16,15 +18,23 @@ public class CardInfo {
     private final Validity validity;
     private final Cvc cvc;
 
-    public String getRawString() {
+    public String getEncryptedString(Uid uid) throws CryptoException {
+        return AES256Crypto.encrypt(getRawString(), uid.getUid());
+    }
+
+    private String getRawString() {
         return String.join(MEMBER_SEPARATOR, cardNumber.getCardNumber(), validity.getValidity(), cvc.getCvc());
     }
 
-    public static CardInfo ofRawString(String rawString){
+    public static CardInfo ofEncryptedString(String encryptedString, Uid uid) throws CryptoException {
+        return ofRawString(AES256Crypto.decrypt(encryptedString, uid.getUid()));
+    }
+
+    private static CardInfo ofRawString(String rawString) {
         String[] rawMembers = rawString.split(MEMBER_SEPARATOR);
         return CardInfo.builder()
-                .cardNumber(new CardNumber(rawMembers[0].substring(0,rawMembers[0].length()-1)))
-                .validity(new Validity(rawMembers[1].substring(0,rawMembers[1].length()-1)))
+                .cardNumber(new CardNumber(rawMembers[0].substring(0, rawMembers[0].length() - 1)))
+                .validity(new Validity(rawMembers[1].substring(0, rawMembers[1].length() - 1)))
                 .cvc(new Cvc(rawMembers[2]))
                 .build();
     }
--- src/test/java/com/teahyuk/payment/ap/domain/CardInfoTest.java
+++ src/test/java/com/teahyuk/payment/ap/domain/CardInfoTest.java
@@ -1,21 +1,46 @@
 package com.teahyuk.payment.ap.domain;
 
+import com.teahyuk.payment.ap.util.CryptoException;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CardInfoTest {
-
     @Test
-    void rawStringTest() {
+    void encryptAndDecryptTest() throws CryptoException {
         CardInfo cardInfo = CardInfo.builder()
                 .cardNumber(CardNumberTest.cardNumber1)
                 .validity(ValidityTest.thisMonthValidity)
                 .cvc(CvcTest.cvc1)
                 .build();
+        Uid keyUid = UidTest.createTestUid("123");
+
+        String encryptedCardInfo1 = cardInfo.getEncryptedString(keyUid);
+        String encryptedCardInfo2 = cardInfo.getEncryptedString(keyUid);
+
+        assertThat(encryptedCardInfo1)
+                .isNotEqualTo(encryptedCardInfo2);
 
         assertThat(cardInfo)
-                .isEqualTo(CardInfo.ofRawString(cardInfo.getRawString()));
+                .isEqualTo(CardInfo.ofEncryptedString(encryptedCardInfo1, keyUid));
+        assertThat(cardInfo)
+                .isEqualTo(CardInfo.ofEncryptedString(encryptedCardInfo2, keyUid));
+    }
+
+    @Test
+    void decryptExpectError() throws CryptoException {
+        CardInfo cardInfo = CardInfo.builder()
+                .cardNumber(CardNumberTest.cardNumber1)
+                .validity(ValidityTest.thisMonthValidity)
+                .cvc(CvcTest.cvc1)
+                .build();
+        Uid keyUid = UidTest.createTestUid("123");
+
+        String encryptedCardInfo = cardInfo.getEncryptedString(keyUid);
+
+        assertThatThrownBy(() -> CardInfo.ofEncryptedString(encryptedCardInfo, UidTest.createTestUid("1")))
+                .isInstanceOf(CryptoException.class);
     }
 
 }
