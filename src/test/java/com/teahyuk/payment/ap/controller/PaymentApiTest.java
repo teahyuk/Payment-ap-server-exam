d0c10899e44966972295b64a48bc43917d3e722d
teahyuk <jtaeh57@gmail.com>
2020-07-02 13:57:38 +0900

    PaymentService api 테스트 목 테스트 로 수정

--- src/test/java/com/teahyuk/payment/ap/controller/PaymentApiTest.java
+++ src/test/java/com/teahyuk/payment/ap/controller/PaymentApiTest.java
@@ -1,13 +1,17 @@
 package com.teahyuk.payment.ap.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teahyuk.payment.ap.domain.vo.uid.Uid;
+import com.teahyuk.payment.ap.domain.vo.uid.UidTest;
+import com.teahyuk.payment.ap.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,13 +22,16 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
-@SpringBootTest
+@WebMvcTest(value = {PaymentController.class, PaymentService.class})
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class PaymentApiTest {
@@ -32,21 +39,28 @@ class PaymentApiTest {
     @Autowired
     private MockMvc mvc;
 
-    private static Stream<Arguments> provideSuccessDto() {
+    @MockBean
+    PaymentService paymentService;
+
+    private static Stream<Arguments> provideValidParam() {
         return Stream.of(
                 Arguments.of("0123456789", "0720", "032", 4, 20000, 100),
                 Arguments.of("0123456789", "0720", "032", 4, 20000, null));
     }
 
     @ParameterizedTest
-    @MethodSource("provideSuccessDto")
-    void postPaymentTest(String cardNumber, String validity, String cvc, int installment, int amount, Integer vat) throws Exception {
+    @MethodSource("provideValidParam")
+    void postSuccessTest(String cardNumber, String validity, String cvc, int installment, int amount, Integer vat) throws Exception {
+        Uid expectUid = UidTest.createTestUid("_resultUid_");
+        given(paymentService.requestPayment(any()))
+                .willReturn(expectUid);
+
         assertRequest(getRequestMap(cardNumber, validity, cvc, installment, amount, vat),
                 status().isOk(),
-                jsonPath("uid").isString());
+                jsonPath("uid", is(expectUid.getUid())));
     }
 
-    private static Stream<Arguments> provideBadRequestDto() {
+    private static Stream<Arguments> provideInvalidParam() {
         return Stream.of(
                 Arguments.of("012345678", "0720", "032", 4, 20000, 100),
                 Arguments.of("0123456789", "0719", "032", 4, 20000, 100),
@@ -64,7 +78,7 @@ class PaymentApiTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideBadRequestDto")
+    @MethodSource("provideInvalidParam")
     void paymentValidationCheck(String cardNumber, String validity, String cvc, Integer installment, Integer amount, Integer vat) throws Exception {
         assertRequest(getRequestMap(cardNumber, validity, cvc, installment, amount, vat),
                 status().isBadRequest());
@@ -92,6 +106,7 @@ class PaymentApiTest {
         ResultActions resultActions = mvc.perform(
                 post("/v1/payment")
                         .content(new ObjectMapper().writeValueAsString(paymentRequest))
+                        .characterEncoding("UTF-8")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print());
         for (ResultMatcher resultMatcher : resultMatchers) {
