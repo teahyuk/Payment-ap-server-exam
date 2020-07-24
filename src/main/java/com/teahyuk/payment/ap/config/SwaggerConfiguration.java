2a9a11d8fe008b8bacaa6309be8b4bce8734c9c9
teahyuk <jtaeh57@gmail.com>
2020-07-24 11:34:38 +0900

    Swagger ui 설정 추가
    - 각 Controller 에 설정 
    - pom 추가
    - config 추가

---
+++ src/main/java/com/teahyuk/payment/ap/config/SwaggerConfiguration.java
@@ -0,0 +1,49 @@
+package com.teahyuk.payment.ap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Profile("local")
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    private static final String API_DOC_TITLE = "Payment rest api";
+    private static final String API_DOC_DESCRIPTION = "Payment 사용을 위한 rest api 문서 입니다.";
+    private static final String VERSION1 = "v1";
+    private static final String TEAHYUK_GITHUB_IO = "https://teahyuk.github.io";
+    private static final String TEAHYUK = "teahyuk";
+
+    private static final String controllerPackageName;
+
+    static {
+        String configPkgName = SwaggerConfiguration.class.getPackage().getName();
+        String projectPkgName = configPkgName.substring(0, configPkgName.lastIndexOf("."));
+        controllerPackageName = projectPkgName + ".controller";
+    }
+
+    @Bean
+    public Docket swaggerApiV1() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage(controllerPackageName))
+                .paths(PathSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false); // 기본으로 세팅되는 200,401,403,404 메시지를 표시 하지 않음
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title(API_DOC_TITLE)
+                .description(API_DOC_DESCRIPTION)
+                .license(TEAHYUK)
+                .licenseUrl(TEAHYUK_GITHUB_IO)
+                .version(VERSION1)
+                .build();
+    }
+}
--- src/main/java/com/teahyuk/payment/ap/controller/CancelController.java
+++ src/main/java/com/teahyuk/payment/ap/controller/CancelController.java
@@ -7,11 +7,15 @@ import com.teahyuk.payment.ap.dto.response.UidResponse;
 import com.teahyuk.payment.ap.exception.BadRequestException;
 import com.teahyuk.payment.ap.repository.CancelRepository;
 import com.teahyuk.payment.ap.service.CancelService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "2. cancel")
 @RestController
 @RequestMapping("/v1/payment")
 @Slf4j
@@ -25,16 +29,24 @@ public class CancelController {
         this.cancelRepository = cancelRepository;
     }
 
+    @ApiOperation(value = "취소 요청",
+            notes = "결제 취소를 요청 한다.")
     @PostMapping("/{uid}/cancel")
-    public ResponseEntity<?> addCancel(@PathVariable Uid uid,
+    public ResponseEntity<?> addCancel(@ApiParam(value = "결제 id", required = true)
+                                       @PathVariable Uid uid,
+                                       @ApiParam(value = "취소 정보", required = true)
                                        @RequestBody CancelRequest cancelRequest) throws BadRequestException {
         return cancelService.requestCancel(cancelRequest.getCancel(uid))
                 .map(UidResponse::new)
                 .responseEntity();
     }
 
+    @ApiOperation(value = "취소 내역 확인",
+            notes = "uid 에 맞는 결제 취소 내역을 조회 한다.")
     @GetMapping("/{paymentUid}/cancel/{cancelUid}")
-    public ResponseEntity<PaymentResponse> getCancel(@PathVariable Uid paymentUid,
+    public ResponseEntity<PaymentResponse> getCancel(@ApiParam(value = "결제 id", required = true)
+                                                     @PathVariable Uid paymentUid,
+                                                     @ApiParam(value = "결제 취소 id", required = true)
                                                      @PathVariable Uid cancelUid) {
         return cancelRepository.findByUid(cancelUid.getUid())
                 .filter(v -> v.getPayment().getUid().equals(paymentUid.getUid()))
