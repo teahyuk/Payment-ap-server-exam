c7a164a664dd4c526290ad22edb4bdee3977ad7d
bri <bri.ghten@kakaopaycorp.com>
2020-12-15 12:00:23 +0900

    test2

--- src/main/java/com/teahyuk/payment/ap/ApApplication.java
+++ src/main/java/com/teahyuk/payment/ap/ApApplication.java
@@ -1,4 +1,4 @@
-package com.teahyuk.payment.ap;
+package com.teahyuk.payment.ap;  
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
